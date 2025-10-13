5f9601a86a86c0aae6ad0970ab85c6b9c016f59b
Kami <josephkiarie561@gmail.com>
2025-10-13 13:07:42 +0300

    set up

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 
 # setup
 from setuptools import find_packages, setup
+from typing import List
+
+E_DOT = "-e ."
+def get_requirements(file_path:str)->List[str]:
+    # returns a list of requirements
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [requirement.replace("\n","") for requirement in requirements]
+
+        if E_DOT in requirements:
+            requirements.remove(E_DOT)
+        
+    return requirements
 
 setup(
     name = "Students Scores",
@@ -8,5 +22,5 @@ setup(
     author = "Kami",
     author_email = "josephkiarie561@gmail.com",
     packages = find_packages(),
-    install_requires = ["pandas", "numpy", "seaborn"]
+    install_requires = get_requirements("requirement.txt")
 )
