112a2b82945a1ce66c5f70f5c454596381cdeacf
Kami <josephkiarie561@gmail.com>
2025-10-13 19:50:44 +0300

    set up

--- setup.py
+++ setup.py
@@ -4,23 +4,22 @@ from setuptools import find_packages, setup
 from typing import List
 
 E_DOT = "-e ."
-def get_requirements(file_path:str)->List[str]:
-    # returns a list of requirements
+def get_requirements(file_path: str) -> List[str]:
+    """Return a clean list of requirements, skipping editable installs."""
     requirements = []
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        [requirement.replace("\n","") for requirement in requirements]
-
-        if E_DOT in requirements:
-            requirements.remove(E_DOT)
-        
+        for line in file_obj:
+            req = line.strip()
+            if req and req != "-e .":
+                requirements.append(req)
     return requirements
 
+
 setup(
-    name = "Students Scores",
+    name = "Students_scores",
     version = "0.0.1",
     author = "Kami",
     author_email = "josephkiarie561@gmail.com",
     packages = find_packages(),
-    install_requires = get_requirements("requirement.txt")
+    install_requires = get_requirements("requirements.txt")
 )
