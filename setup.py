7e44d071a40cd4fb6a54ff2ef29c11460119c18a
Kami <josephkiarie561@gmail.com>
2025-10-13 12:45:11 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+# setup
+from setuptools import find_packages, setup
+
+setup(
+    name = "Students Scores",
+    version = "0.0.1",
+    author = "Kami",
+    author_email = "josephkiarie561@gmail.com",
+    packages = find_packages(),
+    install_requires = ["pandas", "numpy", "seaborn"]
+)
