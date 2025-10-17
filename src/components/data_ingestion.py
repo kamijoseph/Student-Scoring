8d3ea7285f48371d524d66dd0a7ce9b026f3fa89
Kami <josephkiarie561@gmail.com>
2025-10-17 09:44:15 +0300

    data ingestion block

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,3 +13,54 @@ class DataIngestionConfig:
     train_data_path: str=os.path.join("artifacts", "train.csv")
     test_data_path: str=os.path.join("artifacts", "test.csv")
     raw_data_path: str=os.path.join("artifacts", "raw.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("entered the data ingestion method or component.........")
+        
+        try:
+            data = pd.read_csv("notebook/dataset/stud.csv")
+            logging.info("dataset read as dataframe........")
+
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.train_data_path),
+                exist_ok = True
+            )
+
+            data.to_csv(
+                self.ingestion_config.raw_data_path,
+                index = True,
+                header = True
+            )
+
+            logging.info("train test split initiated..........")
+            train_set, test_set = train_test_split(
+                data,
+                test_size = 0.2,
+                random_state = 21
+            )
+
+            train_set.to_csv(
+                self.ingestion_config.train_data_path,
+                index = False,
+                header = True
+            )
+
+            test_set.to_csv(
+                self.ingestion_config.test_data_path,
+                index = False,
+                header = True
+            )
+            
+            logging.info("ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            raise CustomException(e, sys)
