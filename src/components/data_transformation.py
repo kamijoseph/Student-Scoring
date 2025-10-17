7902492afca126febf6b8cd6738f8031beaae239
Kami <josephkiarie561@gmail.com>
2025-10-17 14:53:18 +0300

    dependencies

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -12,6 +12,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
@@ -66,3 +67,44 @@ class DataTransformation:
 
         except Exception as e:
             raise CustomException(e, sys)
+        
+    def initiate_transformation(self, train_path, test_path):
+        try:
+            train_data = pd.read_csv(train_path)
+            test_data = pd.read_csv(test_path)
+
+            logging.info("reading training and testing data completed ..........")
+            logging.info("obtaining preprocessing object .......")
+
+            preprocess_obj = self.get_transformer()
+
+            X_train, y_train = train_data.drop("math_Score", axis=1), train_data["math_score"]
+            X_test, y_test = test_data.drop("math_score", axis=1), test_data["math_score"]
+
+            logging.inf0("applying preprocessor on train and test datasets.............")
+
+            X_train_transformed = preprocess_obj.fit_transform(X_train)
+            X_test_transformed = preprocess_obj.transform(X_test)
+
+            train_array = np.c_[
+                X_train, np.array(y_train)
+            ]
+            test_array = np.c_[
+                X_test, np.array(y_test)
+            ]
+
+            logging.info('saved preprocessing object...............')
+
+            save_object(
+                file_path = self.data_transformation_config.preprocess_obj_path,
+                obj = preprocess_obj
+            )
+
+            return (
+                train_array,
+                test_array,
+                self.data_transformation_config.preprocess_obj_path
+            )
+
+        except:
+            pass
